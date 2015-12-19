59504e34d52d581bf60ad5ef0a61e6930a129dc8
So <sora.chouri@gmail.com>
2015-12-19 22:51:34 +0100

    Added more credits in About menu

--- app/src/main/java/eliteheberg/sora/org/kuizu/AboutActivity.java
+++ app/src/main/java/eliteheberg/sora/org/kuizu/AboutActivity.java
@@ -21,7 +21,8 @@ public class AboutActivity extends AppCompatActivity {
                         "Ksibi Ahmed Ghazi: <a href=\"mailto:ahmed.g.k@hotmail.fr\">ahmed.g.k@hotmail.fr</a><br><br>" +
                         "  - Icons: Windows 8 metro style from <a href=\"https://icons8.com\">https://icons8.com/</a>" +
                         "  - Font PixelFlag by <a href=\"http://nalgames.com/\">Andrew McCluskey (NAL)</a>" +
-                        "  - Font Laura by <a href=\"http://www.cyreal.org/\">Cyreal</a><br><br>"));
+                        "  - Font Laura by <a href=\"http://www.cyreal.org/\">Cyreal</a>"+
+                        "  - Font laffriotnf by <a href=\"http://www.nicksfonts.com/\">Nick Curtis</a>"));
                 tvAbout.startAnimation((Animation) AnimationUtils.loadAnimation(this, R.anim.scroll_animation));
     }
 }
